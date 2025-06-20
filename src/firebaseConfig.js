d457aa95fe2e29882b5fb10f4929979f70ab3028
maxys2025 <melyx2025@gmail.com>
2025-06-20 23:11:13 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,16 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAP7c85W56Z75noLzD_vVin2IoM02DkKjg",
-    authDomain: "piccantometro.firebaseapp.com",
-    projectId: "piccantometro",
-    storageBucket: "piccantometro.firebasestorage.app",
-    messagingSenderId: "716174745933",
-    appId: "1:716174745933:web:f77360b39f8dd58affd704",
-    measurementId: "G-H94S9GV2J5"
-  };
+  apiKey: "AIzaSyAP7c85W56Z75noLzD_vVin2IoM02DkKjg",
+  authDomain: "piccantometro.firebaseapp.com",
+  projectId: "piccantometro",
+  storageBucket: "piccantometro.firebasestorage.app",
+  messagingSenderId: "716174745933",
+  appId: "1:716174745933:web:f77360b39f8dd58affd704",
+  measurementId: "G-H94S9GV2J5"
+};
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
